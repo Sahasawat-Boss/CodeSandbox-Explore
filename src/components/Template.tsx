c8c6d34c5d8fd07158662a633ee36e8e91b6df58
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-22 15:58:09 +0700

    Create Template.tsx

---
+++ src/components/Template.tsx
@@ -0,0 +1,13 @@
+// src/components/ComponentName.tsx
+
+import React from "react";
+
+const ComponentName: React.FC = () => {
+  return (
+    <div>
+      Main
+    </div>
+  );
+};
+
+export default ComponentName;
